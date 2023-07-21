8b4616fd2038e610d6eb210aff32ab39064b93ff
PsychotherapyCentral <psychotherapy.first@gmail.com>
2023-07-21 15:54:09 +0530

    Added Certifications and Badges part

--- assets/js/main.js
+++ assets/js/main.js
@@ -211,18 +211,10 @@
   /**
    * Initiate portfolio lightbox 
    */
-  const portfolioLightbox = GLightbox({
-    selector: '.portfolio-lightbox'
-  });
-
+  
   /**
    * Initiate portfolio details lightbox 
    */
-  const portfolioDetailsLightbox = GLightbox({
-    selector: '.portfolio-details-lightbox',
-    width: '90%',
-    height: '90vh'
-  });
 
   /**
    * Portfolio details slider
